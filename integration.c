84a9a6b739c9a102aaacb9935914416025566944
EDO_DESKTOP <bertoliedoardo99@gmail.com>
2022-09-17 22:59:35 +0200

    Integration algorithm of sine based on taylor expansion stopped at 9th order. Enjoy.

--- integration.c
+++ integration.c
@@ -2,14 +2,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main()
+//Integration from 0 to any positive real number of a general function 
+
+double func(double x)
 {
+ return x - x*x*x / (6) + x*x*x*x*x / (2*3*4*5) - x*x*x*x*x*x*x / (5040) + x*x*x*x*x*x*x*x*x / (5040*8*9) - x*x*x*x*x*x*x*x*x*x*x / (5040*8*9*10);
+}
 
-	
-	printf("Hello World!\n");
+double integrate(double limit, double precision)
+{
+	double result = 0;
+	double x = 0.0000001;
+	while(x < limit)
+	{
+		result += func(x)*precision ;
+	        x += precision;	
+	}
+
+	return result;
+}
 
 
-
-
+int main()
+{	
+	double precision,limit = 0.0;
+	printf("First parameter: limit of integration from zero\nSecond parameter: precision or rectangle base\nNow input parameters:\n");
+	scanf("%lf %lf", &limit, &precision);
+	printf("Risultato: %lf \n ",integrate(limit,precision));
 	return 0;
 }
